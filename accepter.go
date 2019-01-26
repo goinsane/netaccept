151dd8c779cbe664fc7a5df172916dd9f5e22805
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-01-27 01:10:51 +0300

    bugfix for 'close of closed connection' error after calling Close(...) or Shutdown(...)

--- accepter.go
+++ accepter.go
@@ -46,11 +46,11 @@ type connData struct {
 // immediately return nil. Make sure the program doesn't exit and waits
 // instead for Shutdown to return.
 func (a *Accepter) Shutdown(ctx context.Context) (err error) {
-	err = a.l.Close()
 	select {
 	case a.closeCh <- struct{}{}:
 	default:
 	}
+	err = a.l.Close()
 
 	a.connsMu.RLock()
 	for _, c := range a.conns {
@@ -88,11 +88,11 @@ func (a *Accepter) Shutdown(ctx context.Context) (err error) {
 // Close returns any error returned from closing the Accepter's underlying
 // Listener.
 func (a *Accepter) Close() (err error) {
-	err = a.l.Close()
 	select {
 	case a.closeCh <- struct{}{}:
 	default:
 	}
+	err = a.l.Close()
 
 	a.connsMu.RLock()
 	for _, c := range a.conns {
